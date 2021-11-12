81c1da4b6c154089d777cfcad484e271d94e06c8
gabinrn <gabin.neron@epitech.eu>
2021-11-12 15:42:07 +0100

    Auth to front

--- theme01/config/dev.exs
+++ theme01/config/dev.exs
@@ -5,7 +5,8 @@ config :app, App.Repo,
   username: "postgres",
   password: "postgres",
   database: "app_dev",
-  hostname: "localhost",
+  #hostname: "localhost",
+  hostname: "postgres",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
--- webinterface/src/views/Login.vue
+++ webinterface/src/views/Login.vue
@@ -1,91 +1,254 @@
 <template>
+
+    <div>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+            <a href="#" class="navbar-brand">
+                <img src="../assets/logo.png" width="80">
+            </a>
+            <p class="navbar-brand" href="#"> Username : {{ user.username }} </p>
+            <p class="navbar-brand" href="#"> Email : {{ user.email }} </p>
+    </nav>
+
     <section class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <div class="login-panel text-left">
+                    <!-- Redirection -->
                     <div class="alert alert-primary" role="alert" :style="{opacity: isAlertShow ? 1 : 0}">
-                    Login successfully. <small>Waiting for redirect.</small>
-                    <LoadingComponent width="30"></LoadingComponent>
+                        Login successfully. <small>Waiting for redirect.</small>
+                        <LoadingComponent width="30"></LoadingComponent>
                     </div>
-                    <h1 class="display=3 font-weight-bold">Search User</h1>
-                    <p class="font-weight-bold">Search user information.</p>
+                    <!-- End Redirection -->
+                    <div id="usercontener">
+                        <br><br><br>
+                        <h1 class="display=3 font-weight-bold">User Informations </h1>
+                        <p class="font-weight-bold">User information.</p>
+                        <br>
+                            <div class="form-group">
+                                <label for="email" class="input-label"> Email </label>
+                                <input type="email" v-model="emailInput" class="form-control" placeholder="Email">
+                            </div>
+                        <br>
+                            <div class="form-group">
+                                <label for="Password" class="input-label"> Password </label>
+                                <input type="txt" v-model="passwordInput" class="form-control" placeholder="password">
+                            </div>
+                        <br>
+                            <div class="form-group d-flex justify-content-center">
+                                <button class="btn btn-primary w-25" type="submit" @click="appel(emailInput,passwordInput)">Envoyer</button>
+                            </div>
+                        <br>
+                            <div class="form-group d-flex justify-content-center">
+                                <button class="btn btn-primary w-25" type="submit" @click="postUser(emailInput,passwordInput)">Créer</button>
+                            </div>
+                      </div>
                     <br>
-                    <form action="">
-                        <div class="form-group">
-                            <label for="email" class="input-label"> Email </label>
-                            <input type="email" class="form-control" placeholder="Email">
+
+                            <!-- Affihage / delete -->
+                        <div id="modfify" >
+                            <div class="card">
+                                <div class="card-header">
+                                    User Informations
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title"> Check it ! Two options : Delete or put your new informations  </h5> 
+                                    <!-- <label> Username : <input type="text" v-model="user.username" /> </label> 
+                                    <label> Email : <input type="text" v-model="user.email" /> </label>                              -->
+                                    <p class="card-text"> Username : {{ user.password }} </p>
+                                    <p class="card-text">Email : {{ user.email }} </p>
+                                    <a class="btn btn-primary" type="button" @click="deleteUser(emailInput, passwordInput)">Delete </a>
+                                    <br><br>
+                                    <a class="btn btn-primary" type="button" @click="putUser(emailInput, passwordInput)">Modifier </a>
+                            <br><br>        
+                                <!-- Modifier  -->
+                                    <div class="form-group">
+                                        <label for="email" class="input-label"> Email </label>
+                                        <input type="email" v-model="emailInput" class="form-control" placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="Username" class="input-label"> Username </label>
+                                        <input type="txt" v-model="usernameInput" class="form-control" placeholder="username">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <br>
-                        <div class="form-group">
-                            <label for="Username" class="input-label"> Username </label>
-                            <input type="username" class="form-control" placeholder="username">
-                        </div>
-                        <br>
-                        <div class="form-group d-flex justify-content-center">
-                            <button class="btn btn-primary w-25" @click="Login" v-if="!isLoginIn">Search</button>
-                            <button class="btn btn-primary w-25" disabled v-if="isLoginIn"><LoadingComponent width="30"></LoadingComponent></button>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
     </section>
+    </div>
+ 
+
+   
+
 </template>
 
 <script>
 import LoadingComponent from '../components/LoadingComponent.vue'
+import axios from 'axios'
 
 export default {
-    components: {
-        LoadingComponent
-    }, data() {
-        return {
-            isLoginIn: false,
-            isAlertShow: false
-        }
+    components:{
+        LoadingComponent,
+    
+    }, 
+data() {
+      return {
+        user: [{
+          id: 0,
+          usernameInput:'',
+          emailInput:'',
+          username: '',
+          email: ''
+        }]
+      }
     },
+    // gabi1n@gabin.com
     methods: {
-        login() {
-            this.isLoginIn = true
-
-            setTimeout(() => {
-                this.isLoginIn = false
-                this.isAlertShow = true
-                setTimeout(() => this.redirect(), 1000)
-            }, 1000)
-        },
-        redirect() {
-            this.$router.push({name: 'home'})
-        }
-    }
+        async appel(emailInput,passwordInput) {
+             
+            await axios.post("http://localhost:4000/api/signin",
+            {
+                    email : emailInput,
+                    password : passwordInput
+            })
+            .then((response) => {
+                    this.user = response.data.data[0];
+                    window.location.href = "/dashboard"
+                    console.log(this.user);
+                    alert('reussi')
+                })
+            .catch(function (error) {
+                console.log(error);
+            });
+            },
+    
+       async postUser(emailInput,usernameInput) {
+           
+        await axios.post("http://localhost:4000/api/users",
+        {
+            users: {
+                email : emailInput,
+                username : usernameInput
+            }
+        })
+        .then(() => {
+            alert('reussi')
+            })
+        .catch(function (error) {
+            console.log(error);
+        });
+    },
+       async deleteUser(emailInput,usernameInput) {
+        await axios.delete("http://localhost:4000/api/users/" + this.user.id,
+        {
+            users: {
+                email: emailInput,
+                username : usernameInput
+            }
+        })
+        .then(() => {      
+            alert('reussi')     
+        })
+        .catch(function (error) {
+            console.log(error);
+            alert(error)
+        });
+    },
+  
+       async putUser(emailInput,usernameInput) {
+        await axios.put("http://localhost:4000/api/users/" + this.user.id,
+        {
+            users: {
+                email: emailInput,
+                username : usernameInput
+            }
+        })
+        .then(() => { 
+            alert('reussi')          
+        })
+        .catch(function (error) {
+            console.log(error);
+            alert(error);
+        });
+    },
+    },    
 }
+// Travail sur les requêtes
+      // login() {
+        //     this.isLoginIn = true
+        //     setTimeout(() => {
+        //         this.isLoginIn = false
+        //         this.isAlertShow = true
+        //         setTimeout(() => this.redirect(), 1000)
+        //     }, 1000)
+        // },
+        // redirect() {
+        //     this.$router.push({name: 'home'})
+        // }
+//     async getUser(userID) {
+//       let user;
+//       let url;
+//       if (this.usernameDisplay != "" && this.emailDisplay != "") {
+//         if (userID != (null && undefined)) {
+//           url = "http://localhost:4000/api/users/" + userID;
+//         } else {
+//           url ="http://localhost:4000/api/users?email=" + this.emailDisplay + "&username=" + this.usernameDisplay;
+//         }
+//         await axios
+//           .get(url)
+//           .then(function (response) {
+//             user = response.data.data;
+//             console.log(user);
+//           })
+//           .catch(function (error) {
+//             console.log(error);
+//           });
+//         this.userReceived = user;
+//       }
+//     },
+// },
 </script>
 
-<style>
+<style scoped>
     .widget {
         margin: 0;
         height: unset;
     }
     .login-panel {
-        padding: 200px 0;
         position: relative;
-        /* .alert {
+    }
+    .login-panel .alert {
             opacity: 0;
             position: absolute;
             top: 100px;
             width: 100%;
-            &.alert-primary {
-                background-color: #007BFF;
-                color: #fff;
-                border: none;
-                font-size: 10px;
-            }
-            .widget {
-                position: absolute;
-                right: 5px;
-                top: 0;
-                margin: 10px;
-            }
-        } */
+    }
+    .login-panel .alert .alert-primary {
+        background-color: #007BFF;
+        color: #fff;
+        border: none;
+        font-size: 10px;
+    }
+    .login-panel .alert .widget {
+        position: absolute;
+        right: 5px;
+        top: 0;
+        margin: 10px;
+    }
+    #usercontener{
+        border:1px black solid;
+        border-radius: 15px;
+        padding: 0.5px 10px 20px 10px;
+        margin-top: 25px;
+        margin-bottom: 25px;
+    }
+    #modify{
+        border:5px black solid;
+        border-radius: 15px;
+        padding: 30px;
+        margin-bottom: 50px;
     }
 </style>
+
+  
